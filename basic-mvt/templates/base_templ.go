4cebbd3eae2b8f9412412734e529824750a86682
Taj <tjgurwara99@github.com>
2023-12-07 16:57:37 +0000

    Use generic company name instead of my other company

--- basic-mvt/templates/base_templ.go
+++ basic-mvt/templates/base_templ.go
@@ -27,7 +27,7 @@ func Base(content templ.Component) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var2 := `The Crafty Bottle`
+		templ_7745c5c3_Var2 := `Your Company`
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
